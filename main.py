7f89f9becae0f48fdb7ebfb6053d1132f4202bf8
garrip91 <garrip91@gmail.com>
2023-09-18 12:21:35 +0300

    Refactoring all code | part 1

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pygame
 import settings
 from player import Player
+import math
+from map import world_map
+from ray_casting import ray_casting
 
 
 pygame.init()
@@ -16,7 +19,17 @@ while True:
     player.movement()
     sc.fill(settings.BLACK)
 
-    pygame.draw.circle(sc, settings.GREEN, settings.PLAYER_POS, 12)
+    pygame.draw.rect(sc, settings.BLUE, (0, 0, settings.WIDTH, settings.HALF_HEIGHT))
+    pygame.draw.rect(sc, settings.DARKGRAY, (0, settings.HALF_HEIGHT, settings.WIDTH, settings.HALF_HEIGHT))
+
+    ray_casting(sc, player.pos, player.angle)
+
+    # pygame.draw.circle(sc, GREEN, (int(player.x), int(player.y)), 12)
+    # pygame.draw.line(sc, GREEN, player.pos, (player.x + WIDTH * math.cos(player.angle),
+    #                                          player.y + WIDTH * math. sin(player.angle)), 2)
+    # for x,y in world_map:
+    #     pygame.draw.rect(sc, DARKGRAY, (x, y, TILE, TILE), 2)
 
     pygame.display.flip()
-    clock.tick()
+    clock.tick(settings.FPS)
+    # print(clock.get_fps())
--- settings.py
+++ settings.py
@@ -1,21 +1,33 @@
-# game settings:
+import math
+
+# game settings
 WIDTH = 1200
 HEIGHT = 800
 HALF_WIDTH = WIDTH // 2
 HALF_HEIGHT = HEIGHT // 2
+FPS = 60
+TILE = 100
 
+# ray casting settings
+FOV = math.pi / 3
+HALF_FOV = FOV / 2
+NUM_RAYS = 120
+MAX_DEPTH = 800
+DELTA_ANGLE = FOV / NUM_RAYS
+DIST = NUM_RAYS / (2 * math.tan(HALF_FOV))
+PROJ_COEFF = 3 * DIST * TILE
+SCALE = WIDTH // NUM_RAYS
 
-# player settings:
+# player settings
 PLAYER_POS = (HALF_WIDTH, HALF_HEIGHT)
 PLAYER_ANGLE = 0
 PLAYER_SPEED = 2
 
-
-# colors:
+# colors
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 RED = (220, 0, 0)
 GREEN = (0, 220, 0)
-BLUE = (0, 0, 220)
-DARKGRAY = (110, 110, 110)
+BLUE = (0, 0, 255)
+DARKGRAY = (40, 40, 40)
 PURPLE = (120, 0, 120)
